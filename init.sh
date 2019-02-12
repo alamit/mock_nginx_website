721ddb2cd3d71f6031f5eaa13bab3e1f0f13db43
alamit <alamit@student.42.us.org>
2019-02-13 00:06:54 +0100

    fixed init.sh to run on debian

--- init.sh
+++ init.sh
@@ -6,14 +6,14 @@
 #    By: alamit <alamit@student.42.us.org>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/02/12 22:30:40 by alamit            #+#    #+#              #
-#    Updated: 2019/02/12 22:42:47 by alamit           ###   ########.fr        #
+#    Updated: 2019/02/13 00:06:40 by alamit           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/sh
 
 BASEDIR="$( cd "$(dirname "$0")" ; pwd -P )"
-NAME="$(basename "$BASE_DIR")"
+NAME=`$(basename $BASEDIR)`
 
-sed "s/##INSTALL_PATH##/$BASEDIR/g" $BASE_DIR/nginx/conf.template > $NAME
-sed -i '' "s/##NAME##/$NAME/g" $BASE_DIR/nginx/$NAME
+sed "s/\#\#INSTALL_PATH\#\#/\$BASEDIR/g" $BASEDIR/nginx/conf.template > $BASEDIR/nginx/$NAME
+sed -i "s/\#\#NAME\#\#/\$NAME/g" $BASEDIR/nginx/$NAME
